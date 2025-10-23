d91475cc8d060209ce07603de411c97d8c5aec2f
Özkan Çelikkılıç <ozkanclkklc@gmail.com>
2025-10-23 17:44:41 +0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Basit bir scroll animasyonu örneği
-window.addEventListener("scroll", () => {
-  const nav = document.querySelector(".navbar");
-  if (window.scrollY > 50) {
-    nav.style.background = "#111";
-  } else {
-    nav.style.background = "#000";
-  }
-});
