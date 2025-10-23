432a8ef3b3126fa6ef18c7e2fff0fba7d4287249
Özkan Çelikkılıç <ozkanclkklc@gmail.com>
2025-10-23 16:18:33 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Basit bir scroll animasyonu örneği
+window.addEventListener("scroll", () => {
+  const nav = document.querySelector(".navbar");
+  if (window.scrollY > 50) {
+    nav.style.background = "#111";
+  } else {
+    nav.style.background = "#000";
+  }
+});
